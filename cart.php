31deefe421f04cdc2a040272176e1bcb5f9abc2c
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-05-11 15:43:27 +0200

    shopping cart fonctionnel

--- cart.php
+++ cart.php
@@ -1,17 +1,34 @@
 <?php
 session_start();
-var_dump($_SESSION);
 if (isset($_SESSION['loginname'])) {
-require 'inc/head.php';
-?>
-<section class="cookies container-fluid">
-    <div class="row">
-      TODO : Display shopping cart items from $_SESSION here.
-    </div>
-</section>
-<?php require 'inc/foot.php';
+    $productNames = $_SESSION['productname'];
+    $productImages = $_SESSION['productimage'];
+    require 'inc/head.php';
+    ?>
+    <section class="cookies container-fluid">
+        <div class="row">
+            <table class="table table-responsive">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php for ($i = 0; $i < count($productImages); $i++) { ?>
+                    <tr>
+                        <td> <img src="<?=$productImages[$i]?>" class="img-cart"> </td>
+                        <td><?=$productNames[$i]?></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
+    <?php
+    require 'inc/foot.php';
 } else {
-        header("location:../login.php");
+    header("location:../login.php");
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-var_dump($_SESSION);
+if (isset($_POST['productname']) && isset ($_SESSION['loginname'])) {
+$_SESSION['productname'][]  = $_POST['productname'];
+$_SESSION['productimage'][] = $_POST['productimage'];
+}
 require 'inc/head.php';
 ?>
 <section class="cookies container-fluid">
@@ -11,9 +14,13 @@ require 'inc/head.php';
         <figcaption class="caption">
           <h3>Pecan nuts</h3>
           <p>Cooked by Penny !</p>
-          <a  href="?add_to_cart=46" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
+            <form method="POST">
+                <input id="productimage" name="productimage" type="hidden" value="assets/img/product-46.jpg">
+                <input id="productname" name="productname" type="hidden" value="Pecan nuts">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                </button>
+            </form>
         </figcaption>
       </figure>
     </div>
@@ -23,9 +30,13 @@ require 'inc/head.php';
         <figcaption class="caption">
           <h3>Chocolate chips</h3>
           <p>Cooked by Bernadette !</p>
-          <a  href="?add_to_cart=36" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
+            <form method="POST">
+                <input id="productimage" name="productimage" type="hidden" value="assets/img/product-36.jpg">
+                <input id="productname" name="productname" type="hidden" value="Chocolate chips">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                </button>
+            </form>
         </figcaption>
       </figure>
     </div>
@@ -35,21 +46,29 @@ require 'inc/head.php';
         <figcaption class="caption">
           <h3>Chocolate cookie</h3>
           <p>Cooked by Bernadette !</p>
-          <a  href="?add_to_cart=58" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
+            <form method="POST">
+                <input id="productimage" name="productimage" type="hidden" value="assets/img/product-58.jpg">
+                <input id="productname" name="productname" type="hidden" value="Chocolate cookie">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                </button>
+            </form>
         </figcaption>
       </figure>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <figure class="thumbnail text-center">
-        <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+        <img src="assets/img/product-32.jpg" alt="cookies chocolate chips" class="img-responsive">
         <figcaption class="caption">
           <h3>M&M's&copy; cookies</h3>
           <p>Cooked by Penny !</p>
-          <a  href="?add_to_cart=32" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
+            <form method="POST">
+                <input id="productimage" name="productimage" type="hidden" value="assets/img/product-32.jpg">
+                <input id="productname" name="productname" type="hidden" value="M&M's&copy; cookies">
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                </button>
+            </form>
         </figcaption>
       </figure>
     </div>
